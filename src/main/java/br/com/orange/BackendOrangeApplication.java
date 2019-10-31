48bc0d16326f7edf9a13db22b99ad5460cf0b350
Kenji <caio_yariwake@hotmail.com>
2019-10-31 13:40:50 -0300

    Exclusão do teste

--- src/main/java/br/com/orange/BackendOrangeApplication.java
+++ src/main/java/br/com/orange/BackendOrangeApplication.java
@@ -1,14 +1,10 @@
 package br.com.orange;
 
-import java.util.Arrays;
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.orange.domain.Produto;
 import br.com.orange.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -24,10 +20,6 @@ public class BackendOrangeApplication implements CommandLineRunner {
 	@Override
 	public void run (String... args) {
 		
-		Produto p1 = new Produto(null, "Laranja", "A melhor laranja da região", new Date(), 1.00);
-		Produto p2 = new Produto(null, "Pimentão", "O melhor pimentão da região", new Date(), 10.00);
-		Produto p3 = new Produto(null, "Limão", "O melhor limão da região", new Date(), 0.50);
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 }
