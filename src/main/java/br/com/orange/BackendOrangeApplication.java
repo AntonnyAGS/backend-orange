7877030e56b12d9309b93032737844aa766dc7cc
Kenji <caio_yariwake@hotmail.com>
2019-10-31 13:45:59 -0300

    Exclusão teste correto

--- src/main/java/br/com/orange/BackendOrangeApplication.java
+++ src/main/java/br/com/orange/BackendOrangeApplication.java
@@ -1,17 +1,11 @@
 package br.com.orange;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.orange.repositories.ProdutoRepository;
-
 @SpringBootApplication
 public class BackendOrangeApplication implements CommandLineRunner {
-	
-	@Autowired
-	ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendOrangeApplication.class, args);
